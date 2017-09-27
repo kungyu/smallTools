2d94be596554e9a0b22f083b73be4423ed4d1cb7
kung <kung-yu@163.com>
2017-09-27 12:33:55 +0800

    address  optimization

--- address_analysis/addr.php
+++ address_analysis/addr.php
@@ -1,134 +1,46 @@
 <?php
-	$this->load->helper('address.two.php');
+	public function addrs(){
+        $this->load->helper('address.two.php');
         $this->load->helper('address.city.php');
         $province = addrs();
         $citys = city();
-        $qu = '';
-        $province_keys = array_keys($province);
         $addr=$this->input->post('addr');
         $addr_local = $addr;
-        $what_arr = array("。","&#x3002;","，","&#65292;","…","&#8230;",',','.',' ',"\xc2\xa0","\n","\r","\t",'"',"'",'“',"‘",'!','！',':','：',';','`','`','~','～','(',')','（','）','|','|','\\','/','?','>','<','《','》','*','×','#','#','@','@','%','%');
+        $what_arr = array("。","&#x3002;","，","&#65292;","…","&#8230;",',','.',' ',"\xc2\xa0","\n","\r","\t",'"',"'",'“',"‘",'!','！',':','：',';','`','`','~','～','(',')','（','）','|','|','\\','/','?','>','<','《','》','*','×','#','#','@','@','%','%',
+            '名字','姓名','收件人','收货人','收货方','发件人','发货人','发件方','发货方','手机','电话','-','；');
         $what_arr_org = array_pad(array(),count($what_arr),'');
         $addr_orig = str_replace($what_arr,$what_arr_org,$addr);
+        $addr = str_replace('-','',$addr);
         $what_arr_sp = array_pad(array(),count($what_arr),'|');
         $addr =str_replace($what_arr,$what_arr_sp,$addr);
 
-        $sheng_pos = mb_strpos($addr,'省');
-        $sheng = '';
-        if($sheng_pos){
-            $sheng = mb_substr($addr,$sheng_pos-2,3);
-            if(!in_array(mb_substr($sheng,0,mb_strlen($sheng)-1),$province_keys))
-                $sheng = mb_substr($addr,$sheng_pos-3,4);
-            if(!in_array(mb_substr($sheng,0,mb_strlen($sheng)-1),$province_keys))
-                $sheng = '';
-            $addr = str_replace($sheng,'|',$addr);
-        }else{
-            if(strpos($addr,'北京') !== false)
-                $sheng = '北京市';
-            if(strpos($addr,'上海') !== false)
-                $sheng = '上海市';
-            if(strpos($addr,'天津') !== false)
-                $sheng = '天津市';
-            if(strpos($addr,'重庆') !== false)
-                $sheng = '重庆市';
-        }
-
-
-        preg_match_all('/(.{6})市/', $addr, $arr);
-        $shi = '';
-        if(!empty($arr)&&!empty($arr[0]))
-            $shi = $arr[0][0];
-        if(!empty($shi)){
-            $check_status = 0;
-            foreach($province as $province_val){
-                if(in_array(mb_substr($shi,0,mb_strlen($shi)-1),$province_val)){
-                    $check_status = 1;
-                }
-            }
-            if($check_status == 0){
-                $qu  = $shi;
-                $shi = '';
-            }
-        }
-
-        if(empty($shi) && in_array($sheng,array('上海市','北京市','重庆市','天津市'))){
-            $shi = $sheng;
-        }
-
-        if(!empty($shi) && empty($sheng)){
-            foreach($province as $k=>$v){
-                if(in_array(mb_substr($shi,0,mb_strlen($shi)-1),$v)){
-                    $sheng = $k.'省';
-                    $addr = str_replace($k,'',$addr);
-                    break;
-                }
-            }
-        }
-
-        if(empty($shi) && !empty($sheng)){
-            $sheng_key = mb_substr($sheng,0,mb_strlen($sheng)-1);
-            if(isset($province[$sheng_key])){
-                foreach($province[$sheng_key] as $vv){
-                    if(mb_strpos($addr,$vv) !== false){
-                        $shi = $vv.'市';
-                        break;
-                    }
-                }
-            }
-        }
-
-        $addr = str_replace($shi,'|',$addr);
-        if(empty($qu)) {
-            preg_match_all('/(.{6})县/', $addr, $arr);
-
-            if (!empty($arr) && !empty($arr[0]))
-                $qu = $arr[0][0];
-            else {
-                preg_match_all('/(.{6})市/', $addr, $arr);
-                if (!empty($arr) && !empty($arr[0]))
-                    $qu = $arr[0][0];
-                else {
-                    preg_match_all('/(.{6})区/', $addr, $arr);
-                    if (!empty($arr) && !empty($arr[0]))
-                        $qu = $arr[0][0];
-                }
-            }
-            if (empty($qu) && !empty($shi)) {
-                if (isset($citys[$shi])) {
-                    foreach ($citys[$shi] as $vv) {
-                        if (mb_strpos($addr, mb_substr($vv, 0, mb_strlen($vv) - 1)) !== false) {
-                            $qu = $vv;
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-        if((empty($sheng) || empty($shi)) && !empty($qu)){
-            foreach($citys as $city_key => $city_val){
-                $qu_short = mb_substr($qu,0,mb_strlen($qu)-1);
-                if(in_array($qu_short.'区',$city_val) || in_array($qu_short.'县',$city_val) || in_array($qu_short.'市',$city_val)){
-                    $shi = $city_key;
-                    if(empty($sheng))
-                        foreach($province as $province_key => $province_val){
-                            if(in_array(mb_substr($shi,0,mb_strlen($shi)-1),$province_val)){
-                                $sheng = $province_key.'省';
-                                break;
-                            }
-                        }
-                    break;
-                }
-            }
-        }
+        $district_rep = $this->get_district($addr,$province,$citys);
+        $sheng = $district_rep['sheng'];
+        $shi = $district_rep['shi'];
+        $qu = $district_rep['qu'];
+	
         $result['sheng'] = $sheng;
         $result['shi'] = $shi;
         $result['qu'] = $qu;
+        $result['tel'] = '';
         $addr = str_replace($qu,'|',$addr);
 //        preg_match('/[^0-9+]*(?P<tel>(\+86[0-9]{11})|([0-9]{11})|([0-9]{3,4}-[0-9]{7,10}))[^0-9+]*/',$addr,$tels);
         preg_match("/1[34578]\d{9}/", $addr, $tels);
-        $tel=$tels[0];
+        $tel = '';
+
+        if(!empty($tels)) {
+            $tel = $tels[0];
+
+        }else{
+            //0917-6444257
+            preg_match("/0\d{2,3}\-\d{7,8}/", $addr_local, $tels);
+            if(!empty($tels)){
+                $tel = $tels[0];
+            }
+        }
         $result['tel'] = $tel;
-        $addr = str_replace($tel,'|',$addr);
+
+
         $arr_place = array('楼','座','栋','橦','单元','号','室','收','电话','码');
         $arr_place_rep = array_pad(array(),count($arr_place),'|');
         $addr_copy = str_replace($arr_place,$arr_place_rep,$addr);
@@ -214,14 +126,192 @@
       $user_info = array($sheng,$shi,$qu,$tel,$name);
       $user_info_rep = array('','','','','');
       $addr = str_replace($user_info,$user_info_rep,$addr_local);
-        $arr_special = array(',,','..',' ','，，','。',':','：');
+        $arr_special = array(',,','..',' ','，，','。',':','：','，','-',';','；');
         $arr_special_rep = array_pad(array(),count($arr_special),'');
         $addr = str_replace($arr_special,$arr_special_rep,$addr);
-        $none_str = array('收件人','手机','号码','电话');
-        $none_str_rep = array('','','','');
+        $none_str = array('收件人','手机','号码','电话','名字','姓名','发件人','发货人','收货人','地址');
+        $none_str_rep = array_pad(array(),count($none_str),'');;
         $addr = str_replace($none_str,$none_str_rep,$addr);
+        $addr = str_replace($tel,'',$addr);
+	$sheng_sub = mb_substr($sheng,0,mb_strlen($sheng)-1);
+	$shi_sub = mb_substr($shi,0,mb_strlen($shi)-1);
+	$qu_sub = mb_substr($qu,0,mb_strlen($qu)-1);
+	$repg1="/$sheng_sub/";
+	$repg2="/$shi_sub/";
+	$repg3="/$qu_sub/";
+	$addr = preg_replace($repg1,'',$addr,1);
+	$addr = preg_replace($repg2,'',$addr,1);
+	$addr = preg_replace($repg3,'',$addr,1);
+	$addr = preg_replace('/省/','',$addr,1);
+	$addr = preg_replace('/市/','',$addr,1);
+	$qu_repg = $qu_sub = mb_substr($qu,-1);
+	$qu_rep = "/$qu_repg/";
+	$addr = preg_replace($qu_rep,'',$addr,1);
         $result['addmore'] = $addr;
       echo json_encode($result);
       exit;
+    }
+
+
+private function get_district($addr,$province,$citys){
+        $shi = $sheng = $qu = '';
+        $citys_keys = array_keys($citys);
+        preg_match_all('/(.{6})市/', $addr, $arr);
+        if(!empty($arr) && !empty($arr[0])){
+            foreach($arr as $arr_val){
+                if(mb_strpos($arr_val[0],'市') > 0 ){
+                    if($arr_val[0] == '北京市' || $arr_val[0] == '上海市' || $arr_val[0] == '重庆市' || $arr_val[0] == '天津市') {
+                        $sheng = $arr_val[0] ;
+                        $shi = $arr_val[0];
+                        foreach($province[$sheng] as $pro_val){
+                            if(mb_strpos($addr,$pro_val) !== false){
+                                $qu = $pro_val;
+                            }
+                        }
+                        break;
+                    }
+                    if(in_array($arr_val[0],$citys_keys)){
+                        $shi = mb_substr($arr_val[0],0,mb_strlen($arr_val[0])-1);
+                        break;
+                    }
+                }
+            }
+        }
+        if(empty($shi)){
+            preg_match_all('/(.{9})市/', $addr, $arr);
+            if(!empty($arr) && !empty($arr[0])){
+                foreach($arr as $arr_val){
+                    if(mb_strpos($arr_val[0],'市') > 0 ){
+                        if(in_array($arr_val[0],$citys_keys)){
+                            $shi = mb_substr($arr_val[0],0,mb_strlen($arr_val[0])-1);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+
+        if(empty($sheng))
+            foreach($province as $key=>$val){
+                if(empty($sheng) && !empty($shi)) {
+                    if (in_array($shi, $val)) {
+                        $sheng = $key;
+                        $shi = $shi.'市';
+                        break;
+                    }
+                }
+                if(empty($sheng) && empty($shi)){
+                    foreach ($val as $city_item) {
+                        if (mb_strlen($city_item) >= 3)
+                            $city_item_sub = mb_substr($city_item, 0, 2);
+                        else
+                            $city_item_sub = $city_item;
+                        if (mb_strpos($addr, $city_item_sub) !== false) {
+                            $shi = $city_item;
+                            $sheng = $key;
+                            $shi = isset($citys[$shi]) ? $shi : $shi . '市';
+                            foreach ($citys[$shi] as $city_val) {
+                                $city_val_sub = mb_substr($city_val, 0, 2);
+                                if (mb_strpos($addr, $city_val_sub) !== false) {
+                                    $qu = $city_val;
+                                    if ($city_val_sub == '伊宁')
+                                        if (mb_strpos($addr, $city_val) !== false) {
+                                            $qu = $city_val;
+                                        }
+                                    if ($city_val_sub == '临夏')
+                                        if (mb_strpos($addr, $city_val) !== false) {
+                                            $qu = $city_val;
+                                        }
+                                    break;
+                                }
+                            }
+
+                            break;
+                        }
+                    }
+                    if ($shi)
+                        break;
+                }
+            }
+
+        if(empty($sheng)){
+            foreach($province as $province_key => $province_val){
+                $province_sub = mb_substr($province_key,0,2);
+                if(mb_strpos($addr,$province_sub) !== false){
+                    $sheng = $province_key;
+                    break;
+                }
+            }
+        }
+
+        if(empty($shi) && (empty($sheng) || empty($qu))){
+            if(!empty($sheng)){
+                foreach($province[$sheng] as $province_val){
+                    $city_name = isset($citys[$province_val])?$province_val:$province_val.'市';
+                    foreach($citys[$city_name] as $city_item){
+                        $last = mb_substr($city_item, -1);
+                        if (($last == '市' || $last == '县') && mb_strlen($city_item) > 2)
+                            $city_val_sub = mb_substr($city_item, 0, mb_strlen($city_item) - 1);
+                        else
+                            $city_val_sub = mb_substr($city_item, 0, 3);
+                        if (mb_strpos($addr, $city_val_sub) !== false) {
+                            $shi = $city_name;
+                            $qu = $city_item;
+                            break;
+                        }
+                    }
+                    if($shi)
+                        break;
+                }
+            }
+            else {
+                foreach ($citys as $city_key => $city_val_item) {
+                    foreach ($city_val_item as $city_val) {
+                        $last = mb_substr($city_val, -1);
+                        if (($last == '市' || $last == '县') && mb_strlen($city_val) > 2)
+                            $city_val_sub = mb_substr($city_val, 0, mb_strlen($city_val) - 1);
+                        else
+                            $city_val_sub = mb_substr($city_val, 0, 3);
+                        if (mb_strpos($addr, $city_val_sub) !== false) {
+                            $qu = $city_val;
+                            $shi = $city_key;
+                            foreach ($province as $province_key => $province_val) {
+                                if (in_array($shi, $province_val) || in_array(mb_substr($shi, 0, mb_strlen($shi) - 1), $province_val)) {
+                                    $sheng = $province_key;
+                                    break;
+                                }
+                            }
+                        }
+                        if ($qu)
+                            break;
+                    }
+                    if ($qu)
+                        break;
+                }
+            }
+        }
+
+        if(empty($qu) && !empty($shi)){
+
+            foreach ($citys[$shi] as $city_val) {
+                $last = mb_substr($city_val, -1);
+                if (($last == '市' || $last == '县') && mb_strlen($city_val) > 2)
+                    $city_val_sub = mb_substr($city_val, 0, mb_strlen($city_val) - 1);
+                else
+                    $city_val_sub = mb_substr($city_val, 0, 2);
+                if (mb_strpos($addr, $city_val_sub) !== false) {
+                    $qu = $city_val;
+                }
+                if ($qu)
+                    break;
+            }
+        }
+        return compact('sheng','shi','qu');
+    }
+
+
+
+
 
 ?>
